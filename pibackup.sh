6cc796258a3b7cbb85717317d1504e80b07d6e9d
Carlos R <deses12@gmail.com>
2024-09-22 22:38:12 +0200

    Update pibackup.sh

--- pibackup.sh
+++ pibackup.sh
@@ -6,30 +6,30 @@ DATE=$(date '+%Y-%m-%d_%H-%M-%S')
 HOSTNAME=$(hostname)
 BACKUP_IMAGE="$BACKUP_DIR/${HOSTNAME}_backup_$DATE.img"
 
-if [ ! -d "$BACKUP_DIR" ]; then
-    echo "Output folder $BACKUP_DIR does not exist."
-    exit 1
-fi
-
-if [ ! -f "/usr/local/bin/pishrink" ]; then
-    echo "PiShrink not found at /usr/local/bin/pishrink."
-    echo "Please install PiShrink by following the instructions at: https://github.com/Drewsif/PiShrink"
-    exit 1
-else
-    if ! command -v pigz &> /dev/null; then
-        echo "pigz not found. Install pigz by running: apt install pigz"
-        exit 1
-    fi
-fi
-
 log() {
     echo "---$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE" 2>&1
 }
 
-log  "---Creating backup of SD card to $BACKUP_IMAGE..."
+if [ ! -d "$BACKUP_DIR" ]; then
+    log "Output folder $BACKUP_DIR does not exist."
+    exit 1
+fi
+
+if [ ! -f "/usr/local/bin/pishrink" ]; then
+    log "PiShrink not found at /usr/local/bin/pishrink."
+    log "Please install PiShrink by following the instructions at: https://github.com/Drewsif/PiShrink"
+    exit 1
+else
+    if ! command -v pigz &> /dev/null; then
+        log "pigz not found. Install pigz by running: apt install pigz"
+        exit 1
+    fi
+fi
+
+log  "Creating backup of SD card to $BACKUP_IMAGE..."
 sudo dd if=/dev/mmcblk0 of="$BACKUP_IMAGE" bs=1M status=progress >> "$LOG_FILE" 2>&1
 
-log  "---Shrinking the image with PiShrink..."
+log  "Shrinking the image with PiShrink..."
 sudo /usr/local/bin/pishrink -v -z -a "$BACKUP_IMAGE" >> "$LOG_FILE" 2>&1
 
-log  "---Backup complete! Image stored at $BACKUP_IMAGE"
+log  "Backup complete! Image stored at $BACKUP_IMAGE"
